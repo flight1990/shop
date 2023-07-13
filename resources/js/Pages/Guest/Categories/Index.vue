f56eab11db3181b65b2e6b5234b12a460736ddaa
vborisuk <vladimirborisiuk@gmail.com>
2023-07-13 16:47:07 +0300

    Admin Categories Crud; Guest Categories Index & Show;

---
+++ resources/js/Pages/Guest/Categories/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Каталог</h1>
+</template>
+
+<script>
+
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
+export default {
+    name: "Index",
+    layout: GuestLayout
+}
+</script>
---
+++ resources/js/Pages/Guest/Categories/Show.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ category.name }}</h1>
+</template>
+
+<script>
+export default {
+    name: "Show",
+    props: {
+        category: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
